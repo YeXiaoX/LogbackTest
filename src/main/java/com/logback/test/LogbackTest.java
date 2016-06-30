6d89730a2b97fefdfb9e70a304d0afd7638459c3
YeXiaoX <497706143@qq.com>
2016-06-30 17:23:16 +0800

    add project

---
+++ src/main/java/com/logback/test/LogbackTest.java
@@ -0,0 +1,28 @@
+package com.logback.test;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Ivan on 2016/6/29.
+ */
+public class LogbackTest {
+    private static final Logger logger= LoggerFactory.getLogger(LogbackTest.class);
+
+    public static void main(String[] args){
+        if(logger.isDebugEnabled()){
+            logger.debug("slf4j-logback debug message");
+        }
+        if(logger.isInfoEnabled()){
+            logger.debug("slf4j-logback info message");
+        }
+        if(logger.isTraceEnabled()){
+            logger.debug("slf4j-logback trace message");
+        }
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+    }
+}
